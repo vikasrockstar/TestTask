3593d4aa6a2da6c5a34741adef53d74aead0cce7
vikas kumar <vikaskumar@webkorps.com>
2022-10-13 19:02:23 +0530

    code refactoring

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus";
-
 export default class extends Controller {
   static targets = [
     "firstName",
@@ -10,38 +9,92 @@ export default class extends Controller {
     "employer",
     "dateStarted",
     "dateEnded",
+    "firstNameAlert",
+    "lastNameAlert",
+    "emailAlert",
+    "header",
+    "disabledBtn",
+    "saveBtn",
+    "heading",
+    "successMsg",
+    "usersData",
+    "personalDataModal",
+    "employmentForm",
+    "tRow",
+    "userDataTable",
   ];
   initilize() {
     this.personalData = {};
-    this.emailAlert = document.getElementById("alert4");
     this.employerData = {};
     this.id = "";
-    (this.saveBtn = ""), (this.addBtn = "");
+    this.userData = {};
   }
-//   $(function(){
-//     $("#datepicker").datepicker({
-//         dateFormat: "mm/dd/yy"
-//     });
-// });
+
+  async loadUser() {
+    this.baseUrl = `http://127.0.0.1:3000/users`;
+    this.apiHeader = {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    };
+    try {
+      let res = await fetch(`${this.baseUrl}`, {
+        method: "GET",
+        headers: this.apiHeader,
+      });
+      let users = await res.json();
+      this.userData = users;
+      console.log(users?.length)
+      if(users?.length!==0){this.showTable(users);} 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  connect() {
+    this.loadUser();
+  }
+
   launchModal() {
-    const modal = document.getElementById("staticBackdrop");
-    modal.style.display = "block";
+    document.body.classList.add(`modal-open`);
+    document.body.setAttribute(
+      "style",
+      "overflow: hidden; padding-right: 0px;"
+    );
+    this.personalDataModalTarget.style.display = "block";
+    this.userDataTableTarget.style.overflow = "hidden";
+    this.tRowTarget.style.position = "initial";
   }
 
   closeModal() {
-    const modals = document.getElementById("staticBackdrop");
-    modals.style.display = "none";
+    this.personalDataModalTarget.style.display = "none";
+    this.tRowTarget.style.position = "sticky";
+    this.userDataTableTarget.style.overflow = "auto";
+    document.body.classList.remove("modal-open");
+    document.body.removeAttribute("style");
+    document.body.classList.remove("show");
   }
 
   openEmplyModal() {
-    const modal = document.getElementById("employmentForm");
-    modal.style.display = "block";
+    this.tRowTarget.style.position = "initial";
+    document.body.classList.add(`modal-open`);
+    this.userDataTableTarget.style.overflow = "hidden";
+    this.personalDataModalTarget.removeAttribute("data-action");
+    document.body.setAttribute(
+      "style",
+      "overflow: hidden; padding-right: 0px;"
+    );
+    this.employmentFormTarget.style.display = "block";
   }
 
   closeEmplyModal() {
-    const modal = document.getElementById("employmentForm");
-    modal.style.display = "none";
+    this.employmentFormTarget.style.display = "none";
+    this.tRowTarget.style.position = "sticky";
+    this.userDataTableTarget.style.overflow = "auto";
+    document.body.classList.remove("modal-open");
+    document.body.removeAttribute("style");
+    document.body.classList.remove("show");
   }
+
   phoneFormat(input) {
     input = input.replace(/\D/g, "");
     input = input.substring(0, 10);
@@ -62,35 +115,41 @@ export default class extends Controller {
     }
     return input;
   }
+
   convertDateFormat = (dateString) => {
-    const [month, day, year] = dateString.split('/');
-    return  [day, month, year].join('/');
+    const [month, day, year] = dateString.split("/");
+    return [day, month, year].join("/");
   };
 
-  handleChange() {
-    const err = document.getElementById("alert1");
-    const err1 = document.getElementById("alert2");
+  handleFirstName() {
+    this.headerTarget.innerHTML = `<p></p>`;
+    this.firstNameTarget?.value?.length >= 25
+      ? (this.firstNameAlertTarget.style.display = "block")
+      : (this.firstNameAlertTarget.style.display = "none");
+  }
+
+  handleLastName() {
+    this.headerTarget.innerHTML = `<p></p>`;
+    this.lastNameTarget?.value?.length >= 50
+      ? (this.lastNameAlertTarget.style.display = "block")
+      : (this.lastNameAlertTarget.style.display = "none");
+  }
+
+  handleEmail() {
+    this.headerTarget.innerHTML = `<p></p>`;
+    !/.+@.+\.[A-Za-z]+$/.test(this.emailTarget?.value)
+      ? (this.emailAlertTarget.style.display = "block")
+      : (this.emailAlertTarget.style.display = "none");
+  }
+
+  handlePhoneNumber() {
+    this.headerTarget.innerHTML = `<p></p>`;
     const phone = document.getElementById("phone");
-    const header = document.getElementById("header");
-    this.emailAlert = document.getElementById("alert4");
-    this.emailAlert.style.display = "none";
     phone.value = this.phoneFormat(this.phoneTarget.value);
-    header.innerHTML = "<p><p>";
-    if (this.firstNameTarget?.value?.length >= 25) {
-      err.style.display = "block";
-    } else {
-      err.style.display = "none";
-    }
-    if (this.lastNameTarget?.value.length >= 50) {
-      err1.style.display = "block";
-    } else {
-      err1.style.display = "none";
-    }
-    var cleaned = ("" + this.phoneTarget?.value).replace(/\D/g, "");
-    var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
-    if (match) {
-      let phoneNumberString = match[1] + "-" + match[2] + "-" + match[3];
-    }
+  }
+
+  handleChange() {
+    this.headerTarget.innerHTML = `<p></p>`;
     this.personalData = {
       user: {
         first_name: this.firstNameTarget?.value,
@@ -100,47 +159,49 @@ export default class extends Controller {
         phone_number: this.phoneTarget.value,
       },
     };
-    console.log(this.phoneTarget?.value?.length,'value')
   }
 
   async handleSave() {
-    this.emailAlert = document.getElementById("alert4");
-    const header = document.getElementById("header");
     if (
-      this.personalData?.user?.first_name === "" ||this.personalData?.user?.first_name.length >=25 ||this.personalData?.user?.last_name.length >=50 ||
+      this.personalData?.user?.first_name === "" ||
+      this.personalData?.user?.first_name.length >= 25 ||
+      this.personalData?.user?.last_name.length >= 50 ||
       this.personalData?.user?.last_name === "" ||
       this.personalData?.user?.phone_number === "" ||
-      this.personalData === undefined
+      this.personalData === undefined ||
+      !/.+@.+\.[A-Za-z]+$/.test(this.emailTarget?.value)
     ) {
-      header.innerHTML = `<p style="color:red;">please fill all details</p>`;
-    } else if (!/.+@.+\.[A-Za-z]+$/.test(this.personalData?.user.email)) {
-      this.emailAlert.style.display = "block";
+      this.headerTarget.innerHTML = `<p style="color:red;">Please fill all details</p>`;
     } else {
-      this.emailAlert.style.display = "none";
+      this.headerTarget.innerHTML = `<p></p>`;
       try {
-        let res = await fetch("http://127.0.0.1:3000/users", {
+        let res = await fetch(`${this.baseUrl}`, {
           method: "POST",
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-          },
+          headers: this.apiHeader,
           body: JSON.stringify(this.personalData),
         });
         let data = await res.json();
         this.id = data.id;
         if (res?.status === 201) {
-          header.innerHTML = `<p style="color:red;">email has already been taken</p>`;
+          this.userData = [...this.userData, data];
+          this.showTable(this.userData);
           this.closeModal();
           this.openEmplyModal();
           this.personalData = {};
-          document.getElementById("firstName").value = "";
-          document.getElementById("lastName").value = "";
-          document.getElementById("nickName").value = "";
-          document.getElementById("email").value = "";
-          document.getElementById("phone").value = "";
+          this.firstNameTarget.value = "";
+          this.lastNameTarget.value = "";
+          this.nickNameTarget.value = "";
+          this.emailTarget.value = "";
+          this.phoneTarget.value = "";
+        }
+        if (res?.status === 400) {
+          this.headerTarget.innerHTML = `<p style="color:red;">This email has already been taken</p>`;
+          setTimeout(() => {
+            this.headerTarget.innerHTML = `<p"></p>`;
+          }, 1500);
         }
       } catch (error) {
-        console.log(error, "axios err");
+        console.log(error);
       }
     }
   }
@@ -151,56 +212,46 @@ export default class extends Controller {
       this.dateStartedTarget?.value === "" ||
       this.dateEndedTarget?.value === ""
     ) {
-      console.log(this.dateStartedTarget?.value,'data')
-      const emplyHeading = document.getElementById("heading");
-      emplyHeading.innerHTML = `<p style="color:red"></p>`;
+      this.disabledBtnTarget.style.display = "block";
+      this.saveBtnTarget.style.display = "none";
+      this.headingTarget.innerHTML = `<p style="color:red"></p>`;
     } else {
-      const emplyHeading = document.getElementById("heading");
-      const disBtn = document.getElementById("disabledBtn");
-      this.saveBtn = document.getElementById("saveBtn");
-      disBtn.style.display = "none";
-      this.saveBtn.style.display = "block";
-      emplyHeading.innerHTML = `<p></p>`;
+      this.disabledBtnTarget.style.display = "none";
+      this.saveBtnTarget.style.display = "block";
+      this.headingTarget.innerHTML = `<p></p>`;
       this.employerData = {
         employment: {
           employer_name: this.employerTarget?.value,
-          start_date:this.convertDateFormat(this.dateStartedTarget?.value),
-          end_date:this.convertDateFormat(this.dateEndedTarget?.value),
+          start_date: this.convertDateFormat(this.dateStartedTarget?.value),
+          end_date: this.convertDateFormat(this.dateEndedTarget?.value),
         },
       };
     }
-    console.log(this.employerData?.employment,'data')
   }
 
   async handleAddEmp() {
-    const emplyHeading = document.getElementById("heading");
     if (
       this.employerTarget?.value === "" ||
-      this.dateStarted?.value === "" ||
-      this.dateEnded?.value === "" ||
+      this.dateStartedTarget?.value === "" ||
+      this.dateEndedTarget?.value === "" ||
       this.employerData === undefined
     ) {
-      emplyHeading.innerHTML = `<p style="color:red">Please fill all details</p>`;
+      this.headingTarget.innerHTML = `<p style="color:red">Please fill all details</p>`;
     } else {
       try {
-        let res = await fetch(`http://127.0.0.1:3000/users/${this?.id}`, {
+        let res = await fetch(`${this.baseUrl}/${this?.id}`, {
           method: "PUT",
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-          },
+          headers: this.apiHeader,
           body: JSON.stringify(this.employerData),
         });
         if (res?.status === 200) {
-          let successMsg = document.getElementById("successMsg");
-          document.getElementById("employer").value = "";
-          document.getElementById("startDate").value = "";
-          document.getElementById("endDate").value = "";
-          successMsg.style.display = "block";
+          this.employerTarget.value = "";
+          this.dateStartedTarget.value = "";
+          this.dateEndedTarget.value = "";
+          this.successMsgTarget.style.display = "block";
           setTimeout(() => {
-            successMsg.style.display = "none";
+            this.successMsgTarget.style.display = "none";
           }, [2000]);
-          header.innerHTML = `<p style="color:red;">email has already been taken</p>`;
           this.employerData = {};
         }
       } catch (error) {
@@ -210,61 +261,40 @@ export default class extends Controller {
   }
 
   async handleEmpSave() {
-    const emplyHeading = document.getElementById("heading");
     if (
       this.employerTarget?.value === "" ||
       this.dateStarted?.value === "" ||
       this.dateEnded?.value === "" ||
       this.employerData === undefined
     ) {
-      emplyHeading.innerHTML = `<p style="color:red">Please fill all details</p>`;
+      this.headingTarget.innerHTML = `<p style="color:red">Please fill all details</p>`;
     } else {
-      this.saveBtn.style.display = "block";
-      emplyHeading.innerHTML = "<p></p>";
-      const disabledBtn = document.getElementById("disabledBtn");
-      disabledBtn.style.display = "none";
+      this.saveBtnTarget.style.display = "block";
+      this.headingTarget.innerHTML = "<p></p>";
+      this.disabledBtnTarget.style.display = "none";
       try {
-        let res = await fetch(`http://127.0.0.1:3000/users/${this?.id}`, {
+        let res = await fetch(`${this.baseUrl}/${this?.id}`, {
           method: "PUT",
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-          },
+          headers: this.apiHeader,
           body: JSON.stringify(this.employerData),
         });
-        let data = await res.json();
         if (res?.status === 200) {
-          header.innerHTML = `<p style="color:red;">email has already been taken</p>`;
           setTimeout(() => {
-            header.innerHTML = `<p style="color:red;"></p>`;
+            this.headerTarget.innerHTML = `<p></p>`;
           }, [1000]);
-          document.getElementById("employer").value = "";
-          document.getElementById("startDate").value = "";
-          document.getElementById("endDate").value = "";
+          this.employerTarget.value = "";
+          this.dateStartedTarget.value = "";
+          this.dateEndedTarget.value = "";
           this.closeEmplyModal();
-          this.personalData = {};
-          const card = `<h1></h1>`;
-          try {
-            let res = await fetch("http://127.0.0.1:3000/users", {
-              method: "GET",
-              headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": "application/json",
-              },
-            });
-            let users = await res.json();
-            this.show(users);
-          } catch (error) {
-            console.log(error);
-          }
+          this.employerData = {};
         }
       } catch (error) {
-        console.log(error, "axios err");
+        console.log(error);
       }
     }
   }
-  show(data) {
-    let tab = `<tr>
+  showTable(data) {
+    let tab = `<tr data-home-target="tRow" style="position: sticky;top: 0;background: white;">
           <th>Id</th>
           <th>First Name</th>
           <th>Last Name</th>
@@ -279,9 +309,8 @@ export default class extends Controller {
     <td>${user.last_name}</td>
     <td>${user.nickname}</td>
     <td>${user.email}</td>
-    <td>${user.phone_number}</td>
-</tr>`;
+    <td>${user.phone_number}</td></tr>`;
     }
-    document.getElementById("employees").innerHTML = tab;
+    this.usersDataTarget.innerHTML = tab;
   }
 }
